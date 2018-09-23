3846063fbcb34d98c90ce81aa2dddc72642b1311
pandasandeggs <theteatreehouse@gmail.com>
2018-09-23 12:10:27 -0500

    starting over erased everything

--- src/components/App.js
+++ src/components/App.js
@@ -7,45 +7,11 @@ import Filter from './Filter'
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      hogs: hogs
-    }
-  }
-
-  sortHogsByName = () => {
-
-  }
-
-  sortHogsByWeight = () => {
-
-  }
-
-  sortHogsByGrease = () => {
-
-  }
-
-  handleButtonClick = (event) => {
-    if(event.target.id === 'name'){
-      let newHogs = this.sortHogsByName()
-      this.setState({hogs: newHogs})
-    } else if (event.target.id === 'weight'){
-      let heavyHogs = this.sortHogsByWeight()
-      this.setState({hogs: heavyHogs})
-    } else if (event.target.id === 'greased'){
-      let greasyHogs = this.sortHogsByGrease()
-      this.setState({hogs: greasyHogs})
-    }
-  }
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
           < Nav />
-          < HogTile hogs={this.state.hogs} />
-          < Filter buttonFilter={this.handleButtonClick}/>
       </div>
     )
   }
--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -3,8 +3,6 @@ import HogDisplay from './HogDisplay'
 
 export default class HogTile extends Component {
 
-
-
   getHogImage = (hog) => {
     let hogName = hog.name.split(' ').join('_').toLowerCase()
     let hogImage = require(`../hog-imgs/${hogName}.jpg`)
@@ -15,7 +13,7 @@ export default class HogTile extends Component {
     return this.props.hogs.map((hog) => < HogDisplay key={hog.name} hog={hog} image={this.getHogImage(hog)}/>);
   }
 
-  
+  //^^^was 'this.props.hogs' ^^^
 
   render() {
     return(
